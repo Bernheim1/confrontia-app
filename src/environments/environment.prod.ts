842fe0203c37af7b28fc11b56c2e1d1bf907ef23
Bernheim1 <agustinbernheim8@gmail.com>
2026-02-02 16:04:58 -0300

    dp

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://76.13.229.224:5000',
-  basePath: 'http://76.13.229.224:5000/api/',
-  sessionName: 'confrontia',
-  debug: false
+  apiUrl: '/api'
 };
